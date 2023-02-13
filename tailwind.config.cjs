7db2fa9cbaaeddfe49581f6104de50815d396a64
baezdev <baezdev@gmail.com>
2023-02-13 01:37:54 -0600

    Add colors and animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,18 @@ module.exports = {
     extend: {
       colors: {
         primary: "#a259ff",
+        secon: "#ffc700",
+        third: "#ec4899",
+      },
+      keyframes: {
+        dance: {
+          "0%": { transform: "rotate(-8deg)" },
+          "50%": { transform: "rotate(8deg)" },
+          "100%": { transform: "rotate(-8deg)" },
+        },
+      },
+      animation: {
+        dance: "dance .3s linear infinite",
       },
     },
   },
