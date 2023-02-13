0304266bd07859f734604a73a8cca4a31ac82fc9
baezdev <baezdev@gmail.com>
2023-02-13 01:40:42 -0600

    Add links of navbar

---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,40 @@
+import { LinkButton } from "./LinkButton";
+
+export const NavLinks = () => {
+  const links = [
+    {
+      text: "tu invitacion tendra",
+      href: "#content",
+    },
+    {
+      text: "planes",
+      href: "#plans",
+    },
+    {
+      text: "diseños",
+      href: "/designs",
+    },
+    {
+      text: "sobre nosotros",
+      href: "/about",
+    },
+  ];
+
+  return (
+    <ul class="flex gap-7 font-medium capitalize text-lg items-center cursor-pointer">
+      {links.map(({ text, href }) => (
+        <li>
+          <a
+            class="px-1 py-px relative after:content-[''] after:absolute  after:w-0 after:hover:w-full after:h-[3.5px] after:bg-black after:left-0 after:bottom-0 after:rounded after:transition-all after:duration-300"
+            href={href}
+          >
+            {text}
+          </a>
+        </li>
+      ))}
+      <li>
+        <LinkButton text="acceder" href="/login" />
+      </li>
+    </ul>
+  );
+};
