5507fa9e1c57d19b20ee29d4b628d40b901d05e3
baezdev <baezdev@gmail.com>
2023-02-13 01:41:08 -0600

    Add data

---
+++ src/data/events.js
@@ -0,0 +1,30 @@
+export const events = [
+  {
+    eventName: "boda",
+    image: "boda",
+  },
+  {
+    eventName: "xv años",
+    image: "xv",
+  },
+  {
+    eventName: "baby shower",
+    image: "baby-shower",
+  },
+  {
+    eventName: "despedida de solter@",
+    image: "despedida-soltera",
+  },
+  {
+    eventName: "cumpleaños",
+    image: "cumple",
+  },
+  {
+    eventName: "graduaciones",
+    image: "graduacion",
+  },
+  {
+    eventName: "y más...",
+    image: "mas",
+  },
+];
