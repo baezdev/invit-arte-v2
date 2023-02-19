11331cb1f54877aef3d232598d191357b1689f4b
baezdev <baezdev@gmail.com>
2023-02-19 17:02:07 -0600

    fix name

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -17,7 +17,7 @@ export const Testimonials = () => {
     <div class="flex items-center justify-center gap-10">
       <ArrowButton onClick={previusTestimonial} arrowDirection="left" />
       <div class="relative w-[650px] h-72 flex items-center justify-center flex-col text-center rounded-3xl p-5">
-        <p class="text-xl font-semibold mb-5">{name}</p>
+        <p class="text-2xl font-semibold mb-5">{name}</p>
         <p class="text-lg mb-7">{message}</p>
         <div class="flex gap-3">
           {stars.map(() => (
