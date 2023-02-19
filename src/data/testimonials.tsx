e738d3924c30bf529b6dae7c63605cbe62093835
baezdev <baezdev@gmail.com>
2023-02-19 01:37:23 -0600

    add testimonial data

---
+++ src/data/testimonials.tsx
@@ -0,0 +1,16 @@
+import type { Testimonial } from "src/interfaces";
+
+export const testimonials: Array<Testimonial> = [
+  {
+    name: "Cristian",
+    message:
+      "Excelente servicio, siempre me sorprenden con su creatividad. Es la mejor opción en el mercado que vas a poder encontrar. Muchas gracias por siempre darles ese toque a mis eventos que empieza desde la invitación.",
+    rating: 5,
+  },
+  {
+    name: "Angelica",
+    message:
+      "Diseñaron la invitación de mi cumple y me encanto, muy profesionales, entregaron en tiempo y forma. Lo recomiendo si buscas algo original.",
+    rating: 4,
+  },
+];
