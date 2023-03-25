a917a13dec62a62bb4d90b5a84e51cfee736e16d
baezdev <baezdev@gmail.com>
2023-03-24 23:19:03 -0600

    Fix login inputs

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,36 +2,32 @@ import { Button } from "@components/Button";
 import { InputForm } from "@components/form/InputForm";
 import { Email, Lock } from "@components/icons/IconsForm";
 
-const inputs = [
-  {
-    name: "email",
-    label: "Email o Número de teléfono",
-    type: "email",
-    holder: "ejemplo@correo.com",
-    icon: <Email />,
-  },
-  {
-    name: "password",
-    label: "Contraseña",
-    type: "password",
-    holder: "Minimo 8 caracteres",
-    icon: <Lock />,
-  },
-];
-
-const LoginForm = () => {
+export const LoginForm = () => {
   return (
     <form className="mt-5">
-      {inputs.map((inp) => (
-        <div className="mb-5" key={inp.name}>
-          <InputForm {...inp} />
-        </div>
-      ))}
+      <div className="mb-5">
+        <InputForm
+          name="email"
+          type="email"
+          label="Email o Número de teléfono"
+          holder="ejemplo@correo.com"
+          icon={<Email />}
+          done
+        />
+      </div>
+      <div className="mb-5">
+        <InputForm
+          name="password"
+          type="password"
+          label="Contraseña"
+          holder="Minimo 8 caracteres"
+          icon={<Lock />}
+          done
+        />
+      </div>
       <div className="mt-9">
         <Button>Acceder</Button>
       </div>
     </form>
   );
 };
-
-export default LoginForm;
