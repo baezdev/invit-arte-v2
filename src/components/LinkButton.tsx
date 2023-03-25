b5a043c82fcfea31f231a86f8b13a2546095e636
baezdev <baezdev@gmail.com>
2023-03-24 21:22:37 -0600

    Changel class for classname

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -6,7 +6,7 @@ interface Props {
 export const LinkButton = ({ text, href }: Props) => {
   return (
     <a
-      class="bg-primary leading-none inline-block font-semibold text-white px-6 py-4 rounded-3xl transition ease-in-out hover:scale-110 duration-300 capitalize"
+      className="bg-primary leading-none inline-block font-semibold text-white px-6 py-4 rounded-3xl transition ease-in-out hover:scale-110 duration-300 capitalize"
       href={href}
     >
       {text}
